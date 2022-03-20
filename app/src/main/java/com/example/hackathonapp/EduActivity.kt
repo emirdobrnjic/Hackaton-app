1b6a1a07b547eb7e0a278275e7a30611437230dc
emirdobrnjic <78639693+emirdobrnjic@users.noreply.github.com>
2022-03-20 12:54:28 +0100

    prva slika add - aktiviti za provjeru atologina i ostalog dodan skener za provjeru qr koda login implementacija signup
    PRAVI INTENT dodan ( main aktiviti sto aplikacija zapravo i radi)
    POKUSAJ BROJACA

--- app/src/main/java/com/example/hackathonapp/EduActivity.kt
+++ app/src/main/java/com/example/hackathonapp/EduActivity.kt
@@ -1,11 +1,20 @@
 package com.example.hackathonapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class EduActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edu)
+
+        val button = findViewById<Button>(R.id.button11)
+        button.setOnClickListener{
+            val intent = Intent(this, PraviAktiviti::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
